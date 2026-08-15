7fa1a4bcd027e4ca9c0d001d45d16374642d47c0
doomed-phobos <the_ekj.2121@outlook.com>
2026-08-15 14:33:51 -0500

    feat(UI): added hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import hero from '@/assets/hero.mp4';
+
+function Hero() {
+  return <>
+    <div className="relative h-screen p-3">
+      <video
+        src={hero}
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="h-full w-full rounded-[1rem] object-cover brightness-60"
+      />
+
+      <div className="absolute inset-20 flex items-center grid grid-cols-2">
+        <div className="flex flex-col gap-2">
+          <h1>
+            Video & Motion Designer
+          </h1>
+          <h4>
+            Crafting premium motion experiences for brands and studios. From concept to delivery - clean, modern, and polished.
+          </h4>
+          <h3>
+            <span className="font-geist">—</span> BYMIKE
+          </h3>
+        </div>
+      </div>
+    </div>
+  </>;
+}
+
 function App() {
   return (
     <>
-
+      <Hero/>
     </>
   )
 }
